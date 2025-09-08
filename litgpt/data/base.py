Fixing the graph_positional_encoding calculation in the base.py

litgpt/data/base.py
@@ -10,6 +10,7 @@ from torch.utils.data import Dataset
 
 from litgpt.prompts import PromptStyle
 from litgpt.tokenizer import Tokenizer
+from litgpt.utils import graph_positional_encoder
 
 
 class DataModule(LightningDataModule):
@@ -17,7 +18,12 @@ class DataModule(LightningDataModule):
 
     @abstractmethod
     def connect(
-        self, tokenizer: Optional[Tokenizer] = None, batch_size: int = 1, max_seq_length: Optional[int] = None
+        self,
+        tokenizer: Optional[Tokenizer] = None,
+        batch_size: int = 1,
+        max_seq_length: Optional[int] = None,
+        n_embd: Optional[int] = None,
+        k: Optional[int] = None,
     ) -> None:
         """All settings that can't be determined at the time of instantiation need to be passed through here
         before any dataloaders can be accessed.
@@ -60,6 +66,8 @@ class SFTDataset(Dataset):
         mask_prompt: bool = True,
         ignore_index: int = -100,
         transform: Optional[Callable[[Any], Any]] = None,
+        n_embd: Optional[int] = None,
+        k: Optional[int] = None,
     ) -> None:
         self.data = data
         self.tokenizer = tokenizer
@@ -70,6 +78,8 @@ class SFTDataset(Dataset):
         self.mask_prompt = mask_prompt
         self.ignore_index = ignore_index
         self.transform = transform
+        self.n_embd = n_embd
+        self.k = k
 
     def __len__(self) -> int:
         return len(self.data)
@@ -94,7 +104,7 @@ class SFTDataset(Dataset):
             encoded_response
         )
 
-        return {
+        item = {
             "input_ids": encoded_prompt_and_response,
             "labels": labels,
             "token_counts": {
@@ -102,6 +112,14 @@ class SFTDataset(Dataset):
                 "raw_plus_prompt_template": len(encoded_prompt_and_response),
             },
         }
+
+        if linearized_graph := example.get("linearized_graph"):
+            with torch.no_grad():
+                gpe = graph_positional_encoder(linearized_graph, self.n_embd, self.k)
+            gpe = gpe.detach().cpu()
+            item["graph_positional_encodings"] = gpe
+
+        return item
 
 
 def get_sft_collate_fn(max_seq_length: int = -1, pad_id: int = 0, ignore_index: int = -100):
@@ -129,6 +147,13 @@ def _sft_collate_fn(
         # Truncate if needed
         if max_seq_length > 0:
             batched[key] = batched[key][:, :max_seq_length]
+
+    try:
+        batched["graph_positional_encodings"] = torch.nn.utils.rnn.pad_sequence(
+            [sample["graph_positional_encodings"] for sample in samples], batch_first=True, padding_value=pad_id
+        )
+    except KeyError:  # In cases where the dataset doesn’t have graph_positional_encodings
+        pass
 
     batched["token_counts"] = {}
     batched["token_counts"]["raw"] = torch.tensor(  # Token count without padding and without prompt template

litgpt/data/molhiv.py
@@ -13,6 +13,7 @@ from torch_geometric.data.storage import GlobalStorage
 from litgpt.data import DataModule, SFTDataset, get_sft_collate_fn
 from litgpt.prompts import PromptStyle
 from litgpt.tokenizer import Tokenizer
+from litgpt.utils import is_valid_smiles
 
 
 @dataclass
@@ -33,6 +34,8 @@ class MolHIV(DataModule):
     max_seq_length: int = field(default=-1, init=False, repr=False)
     train_dataset: Optional[SFTDataset] = field(default=None, init=False, repr=False)
     val_dataset: Optional[SFTDataset] = field(default=None, init=False, repr=False)
+    n_embd: int = field(default=None, init=False, repr=False)
+    k: int = field(default=None, init=False, repr=False)
 
     def __post_init__(self) -> None:
         super().__init__()
@@ -40,11 +43,18 @@ class MolHIV(DataModule):
             self.prompt_style = PromptStyle.from_name(self.prompt_style)
 
     def connect(
-        self, tokenizer: Optional[Tokenizer] = None, batch_size: int = 1, max_seq_length: Optional[int] = None
+        self,
+        tokenizer: Optional[Tokenizer] = None,
+        batch_size: int = 1,
+        max_seq_length: Optional[int] = None,
+        n_embd: Optional[int] = None,
+        k: Optional[int] = None,
     ) -> None:
         self.tokenizer = tokenizer
         self.batch_size = batch_size
         self.max_seq_length = -1 if max_seq_length is None else max_seq_length
+        self.n_embd = n_embd
+        self.k = k
 
     def prepare_data(self) -> None:
         torch.serialization.add_safe_globals([DataEdgeAttr])
@@ -60,7 +70,7 @@ class MolHIV(DataModule):
         list_data = [
             {
                 "instruction": "Classify the provided HIV molecule into \"active\" or \"inactive\"",
-                "input": smile,
+                "linearized_graph": smile,
                 "output": activity
             }
             for smile, activity in zip(smiles, activities)
@@ -77,8 +87,9 @@ class MolHIV(DataModule):
         valid_idx = pd.read_csv(f"{split_dirt}/valid.csv.gz").squeeze("columns").values
         # test_idx = pd.read_csv(f"{split_dirt}/test.csv.gz").squeeze("columns").values
 
-        train_data = [data[i] for i in train_idx]
-        val_data = [data[i] for i in valid_idx]
+        train_data = [data[i] for i in train_idx if is_valid_smiles(data[i]["linearized_graph"])]
+        val_data = [data[i] for i in valid_idx if is_valid_smiles(data[i]["linearized_graph"])]
+        # test_data = [data[i] for i in test_idx if is_valid_smiles(data[i]["linearized_graph"])]
 
         self.train_dataset = SFTDataset(
             data=train_data,
@@ -87,6 +98,8 @@ class MolHIV(DataModule):
             max_seq_length=self.max_seq_length,
             mask_prompt=self.mask_prompt,
             ignore_index=self.ignore_index,
+            n_embd=self.n_embd,
+            k=self.k,
         )
 
         self.val_dataset = SFTDataset(
@@ -96,6 +109,8 @@ class MolHIV(DataModule):
             max_seq_length=self.max_seq_length,
             mask_prompt=self.mask_prompt,
             ignore_index=self.ignore_index,
+            n_embd=self.n_embd,
+            k=self.k,
         )
 
     def train_dataloader(self) -> DataLoader:

litgpt/utils.py
@@ -922,7 +922,15 @@ def kill_process_tree(pid: int):
         pass  # Process already exited
 
 
-def graph_positional_encoder(smiles: str, n_embd: int, k: int = 20) -> torch.Tensor:
+def is_valid_smiles(smiles: str) -> bool:
+    try:
+        read_smiles(smiles, strict=True)
+        return True
+    except (KeyError, ValueError):
+        return False
+
+
+def graph_positional_encoder(smiles: str, n_embd: int, k: int) -> torch.Tensor:
     hiv_mol = read_smiles(smiles)
 
     """ Adjacency matrix """
@@ -945,6 +953,8 @@ def graph_positional_encoder(smiles: str, n_embd: int, k: int = 20) -> torch.Ten
     MLP = nn.Sequential(nn.Linear(2 * k, n_embd), nn.GELU())
 
     PE = torch.from_numpy(PE).float()  # (2k, n)
-    HivPE = MLP(PE)  # (n, n_embd)
 
-    return HivPE
+    with torch.no_grad():  # forward pass without tracking
+        HivPE = MLP(PE)  # (n, n_embd)
+
+    return HivPE.detach()